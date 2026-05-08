1f4e1d4f15cc37a0fba7921516bc660666c9757f
Victor Gabriel <victorgabriel27021@gmail.com>
2026-05-08 18:09:19 -0300

    Rails curse - db and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cadastrando moedas!..."
+
+Coin.create!(
+	description: "Bitcoin",
+	acronym: "BTC",
+	url_image: "https://png.pngtree.com/png-vector/20240827/ourmid/pngtree-gold-bitcoin-cryptocurrency-coin-3d-illustration-png-image_13630971.png"
+)
+
+Coin.create!(
+	description: "Ethereum",
+	acronym: "ETH",
+	url_image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+)
+
+Coin.create!(
+	description: "DASH",
+	acronym: "DASH",
+	url_image: "https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png"
+)
+
+puts "Moedas cadastradas!"
