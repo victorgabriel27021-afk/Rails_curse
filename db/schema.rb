428b34d3f766e0550bce1c6a9b4b674e86750dbf
Victor Gabriel <victorgabriel27021@gmail.com>
2026-05-13 16:14:31 -0300

    Rails curse - Criando 2o Crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_30_190353) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_13_185554) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "description"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
